8f4ff2295018420fc73d131d51b03aba66c428a3
Dasha Ostrikova <dashka.ostrikova@gmail.com>
2021-02-26 22:57:20 +0300

    feat: implement min, max, avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array=[]) {
+  const arr = array.sort((x,y) => x-y)
+  return arr[0] || 0;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (array=[]) {
+  const arr = array.sort((x,y) => x-y)
+  return arr[array.length-1] || 0;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (array=[]) {
+  const sum = array.reduce((summa,digit) => summa + digit,0)
+  return sum / array.length || 0;
 }
